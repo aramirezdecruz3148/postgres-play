0d415a196348269020d458f211a5940a7be230b5
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-09 14:58:50 -0700

    created my get route with a passing test

--- test/dogs.test.js
+++ test/dogs.test.js
@@ -41,4 +41,20 @@ describe('dog routes', () => {
         testDog(body);
       });
   });
+
+  it('gets all dogs', () => {
+    return Promise.all([
+      createDog({ name: 'Barkley', breed: 'Doodle', is_certified: false }),
+      createDog({ name: 'Clementine', breed: 'Great Dane', is_certified: true }),
+      createDog({ name: 'Tunes', breed: 'Mutt', is_certified: true })
+    ])
+      .then(() => {
+        return request(app)
+          .get('/api/v1/dogs')
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.length).toBe(3);
+          });
+      });
+  });
 });
